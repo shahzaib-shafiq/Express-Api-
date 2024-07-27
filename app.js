0addd6286814491bcf901b8ef78eacb994713f44
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-27 16:23:12 +0500

    handling errors in express

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const app = express();
+var morgan = require("morgan");
+
 const port = 3000;
 
+app.use(morgan("dev"));
 const productRoutes = require("./api/Routes/Products");
 const ordersRoutes = require("./api/Routes/orders");
 
 app.use("/products", productRoutes);
 
 app.use("/orders", ordersRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

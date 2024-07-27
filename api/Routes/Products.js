761a72092fda61b5b0dfba6445c0e8524a52f499
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-27 15:44:13 +0500

    handling put,patch,del request

--- api/Routes/Products.js
+++ api/Routes/Products.js
@@ -22,6 +22,28 @@ router.get("/:productId", (req, res, next) => {
   });
 });
 
+router.delete("/:productId", (req, res, next) => {
+  const productId = req.params.productId;
+  res.status(200).json({
+    message: "Handling Delete Requests to /products",
+  });
+});
+
+router.put("/:productId", (req, res, next) => {
+  const productId = req.params.productId;
+  res.status(200).json({
+    message: "Handling PUT Requests to /products",
+  });
+});
+
+router.patch("/:productId", (req, res, next) => {
+  const productId = req.params.productId;
+  res.status(200).json({
+    message: "Handling PATCH Requests to /products",
+    id: productId,
+  });
+});
+
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
 

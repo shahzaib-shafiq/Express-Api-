4d68b9ed84b2c3f74551973613de1fa3fa9b1bcd
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-28 12:23:59 +0500

    get request to fetch all Orders from

--- api/Routes/orders.js
+++ api/Routes/orders.js
@@ -5,17 +5,22 @@ const mongoose = require("mongoose");
 const Order = require("../Models/orders");
 const product = require("../Models/product");
 
-router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "order were fetched from server",
-  });
-});
-
 // router.get("/", (req, res, next) => {
 //   res.status(200).json({
 //     message: "order were fetched from server",
 //   });
 // });
+
+router.get("/", (req, res, next) => {
+  Order.find()
+    .exec()
+    .then((docs) => {
+      res.status(200).json(docs);
+    })
+    .catch((err) => {
+      res.status(404).json({ error: err });
+    });
+});
 
 router.get("/:orderId", (req, res, next) => {
   const orderId = req.params.orderId;

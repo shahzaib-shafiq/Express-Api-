2e9f35486f6d96bcd49374a497cbd40170512324
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-28 18:55:18 +0500

    api to delete a user

--- api/Routes/users.js
+++ api/Routes/users.js
@@ -45,4 +45,21 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
+router.delete("/:userId", (req, res, next) => {
+  User.remove({ _id: req.params.userId })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "User deleted",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+module.exports = router;
 module.exports = router;
